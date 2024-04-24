5f2b03172bc7581f25e701f1fa7dddb92c6bb411
Asmit tyagi <ashmittyagigms@gmail.com>
2024-04-24 17:51:56 +0530

    add reset button in accordian

--- src/components/randomColorGenerator/index.jsx
+++ src/components/randomColorGenerator/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useState } from "react";
 
 function RandomColorGenerator() {
     const [typeOfColor, setTypeOfColor] = useState("hex");
